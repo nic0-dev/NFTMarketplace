438c1e61c1c570c69f5967e2cb0b576272f34296
Mark Cagas <90206115+nic0-dev@users.noreply.github.com>
2024-03-02 06:48:55 +0000

    Initialize Contract and Hardhat files

---
+++ contracts/NFTMarketplace.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFTMarketplace is ERC721URIStorage, Ownable {
+    constructor() ERC721("NFTMarketplace, "NFTM") Ownable(msg.sender) {}
+}
